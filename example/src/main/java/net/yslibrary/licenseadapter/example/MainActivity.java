cc549fe3660afd40189b1a77d4ab0ca6f1f345ba
yshrsmz <the.phantom.bane@gmail.com>
2016-04-24 23:30:37 +0900

    update example

--- example/src/main/java/net/yslibrary/licenseadapter/example/MainActivity.java
+++ example/src/main/java/net/yslibrary/licenseadapter/example/MainActivity.java
@@ -23,28 +23,31 @@ public class MainActivity extends AppCompatActivity {
         "https://developer.android.com/sdk/terms.html"));
     licenses.add(Licenses.noContent("Fabric", "Twitter", "https://fabric.io/terms"));
     licenses.add(Licenses.fromGitHub("jakewharton/butterknife"));
-    licenses.add(Licenses.fromGitHub("gabrielemariotti/changeloglib"));
+    licenses.add(Licenses.noContent("ChangeLog Library", "Gabriele Mariotti",
+        "https://github.com/gabrielemariotti/changeloglib"));
     licenses.add(Licenses.fromGitHub("hdodenhof/CircleImageView"));
     licenses.add(Licenses.fromGitHub("google/dagger"));
     licenses.add(Licenses.fromGitHub("bumptech/glide", Licenses.LICENSE_NAME_BSD,
         Licenses.LICENSE_NO_EXTENSION));
     licenses.add(Licenses.fromGitHub("evant/gradle-retrolambda"));
-    licenses.add(Licenses.fromGitHub("google/gson"));
-    licenses.add(Licenses.fromGitHub("immutables/immutables"));
+    licenses.add(Licenses.fromGitHub("google/gson", Licenses.LICENSE_NO_EXTENSION));
+    licenses.add(Licenses.fromGitHub("immutables/immutables", Licenses.LICENSE_NO_EXTENSION));
     licenses.add(
         Licenses.fromGitHub("jhy/jsoup", Licenses.LICENSE_NAME_MIT, Licenses.LICENSE_NO_EXTENSION));
     licenses.add(Licenses.fromGitHub("square/leakcanary"));
     licenses.add(Licenses.fromGitHub("square/okhttp"));
-    licenses.add(Licenses.fromGitHub("realm/realm-java"));
-    licenses.add(Licenses.fromGitHub("yqritc/RecyclerView-MultipleViewTypesAdapter"));
-    licenses.add(Licenses.fromGitHub("ReactiveX/RxAndroid"));
-    licenses.add(Licenses.fromGitHub("ReactiveX/RxJava"));
+    licenses.add(Licenses.fromGitHub("realm/realm-java", Licenses.LICENSE_NO_EXTENSION));
+    licenses.add(Licenses.fromGitHub("yqritc/RecyclerView-MultipleViewTypesAdapter",
+        Licenses.LICENSE_NO_EXTENSION));
+    licenses.add(Licenses.fromGitHub("yshrsmz/LicenseAdapter", Licenses.LICENSE_NO_EXTENSION));
+    licenses.add(Licenses.fromGitHub("ReactiveX/RxAndroid", Licenses.LICENSE_NO_EXTENSION));
+    licenses.add(Licenses.fromGitHub("ReactiveX/RxJava", Licenses.LICENSE_NO_EXTENSION));
     licenses.add(Licenses.fromGitHub("facebook/stetho", Licenses.LICENSE_NAME_BSD,
         Licenses.LICENSE_NO_EXTENSION));
     licenses.add(Licenses.fromGitHub("jakewharton/threetenabp"));
     licenses.add(Licenses.fromGitHub("jakewharton/timber"));
     licenses.add(Licenses.fromGitHub("grandcentrix/tray"));
-    licenses.add(Licenses.fromGitHub("twitter/twitter-text"));
+    licenses.add(Licenses.fromGitHub("twitter/twitter-text", Licenses.LICENSE_NO_EXTENSION));
 
     LicenseAdapter adapter = new LicenseAdapter(licenses);
     RecyclerView list = (RecyclerView) findViewById(R.id.list);
