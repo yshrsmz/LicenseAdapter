c19f793eb5d8dbd489ad9ef9e251e360a2e78f7c
yshrsmz <the.phantom.bane@gmail.com>
2016-04-29 17:13:05 +0900

    :memo: add document for Licenses

--- library/src/main/java/net/yslibrary/licenseadapter/Licenses.java
+++ library/src/main/java/net/yslibrary/licenseadapter/Licenses.java
@@ -18,23 +18,48 @@ public class Licenses {
   private static final String DEF_BRANCH = "master";
   private static final String DEF_LICENSE_NAME = LICENSE_NAME_APACHE_V2;
 
+  /**
+   * predefined LicenseEntry for Apache License v2.0
+   */
   public static final License LICENSE_APACHE_V2 =
       new License(DEF_LICENSE_NAME, "http://www.apache.org/licenses/LICENSE-2.0");
 
+  /**
+   * create LicenseEntry from GitHub repository url.
+   * Regard license as Apache v2, and License text file name as "LICENSE.txt"
+   *
+   * @param gitRepo target library's GitHub repository. should be "user/repoName"
+   * @return GitHubLicenseEntry
+   */
   public static GitHubLicenseEntry fromGitHub(String gitRepo) {
     return new GitHubLicenseEntry(DEF_LICENSE_NAME, gitRepo, DEF_BRANCH, null, LICENSE_TXT);
   }
 
+  /**
+   * create LicenseEntry from GitHub repository url & License text path.
+   * Regard license as Apache v2
+   *
+   * @param gitRepo        target library's GitHub repository. should be "user/repoName"
+   * @param relLicensePath relative path to the license file. you can use predefined {@link
+   *                       Licenses.LICENSE_TXT}, {@link Licenses.LICENSE_MD}, {@link
+   *                       Licenses.LICENSE_NO_EXTENSION}
+   * @return GitHubLicenseEntry
+   */
   public static GitHubLicenseEntry fromGitHub(String gitRepo, String relLicensePath) {
     return new GitHubLicenseEntry(DEF_LICENSE_NAME, gitRepo, DEF_BRANCH, null, relLicensePath);
   }
 
+  /**
+   * create LicenseEntry from GitHub repository url & user define {@link License} instance.
+   * This method can be used when the repository does not provide license text file.
+   * You can use predefined {@link Licenses.LICENSE_APACHE_V2} or your own implementation.
+   *
+   * @param gitRepo target library's GitHub repository. should be "user/repoName"
+   * @param license user defined {@link License} instance
+   * @return GitHubLicenseEntry
+   */
   public static GitHubLicenseEntry fromGitHub(String gitRepo, License license) {
     return new GitHubLicenseEntry(null, gitRepo, DEF_BRANCH, license, null);
-  }
-
-  public static NoContentLicenseEntry noContent(String name, String author, String link) {
-    return new NoContentLicenseEntry(name, author, link);
   }
 
   public static GitHubLicenseEntry fromGitHub(String gitRepo, String licenseName,
@@ -42,6 +67,25 @@ public class Licenses {
     return new GitHubLicenseEntry(licenseName, gitRepo, DEF_BRANCH, null, relLicensePath);
   }
 
+  /**
+   * Create LicenseEntry from provided name, author and link.
+   * This method can be used when the library is not hosted on GitHub, and does not provide license
+   * info.
+   *
+   * @param name   Library's name
+   * @param author author's name
+   * @param link   url for the library hosting page
+   * @return NoContentLicenseEntry
+   */
+  public static NoContentLicenseEntry noContent(String name, String author, String link) {
+    return new NoContentLicenseEntry(name, author, link);
+  }
+
+  /**
+   * Load provided licenses
+   *
+   * @param entries list of LicenseEntries to load
+   */
   public static void load(List<LicenseEntry> entries) {
     new LoadLicenseTask().execute(entries.toArray(new LicenseEntry[entries.size()]));
   }
