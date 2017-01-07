2c98f6630648d9949106c29bb2e31a4c2637faa6
yshrsmz <the.phantom.bane@gmail.com>
2017-01-07 11:31:08 +0900

    reorder example

--- example/src/main/java/net/yslibrary/licenseadapter/example/MainActivity.java
+++ example/src/main/java/net/yslibrary/licenseadapter/example/MainActivity.java
@@ -20,26 +20,34 @@ public class MainActivity extends AppCompatActivity {
 
     List<LicenseEntry> licenses = new ArrayList<>();
 
+    // libraries not hosted on GitHub(and you chose not to show license text)
     licenses.add(Licenses.noContent("Android SDK", "Google Inc.",
         "https://developer.android.com/sdk/terms.html"));
     licenses.add(Licenses.noContent("Fabric", "Twitter", "https://fabric.io/terms"));
-    licenses.add(Licenses.fromGitHubApacheV2("jakewharton/butterknife"));
 
     // this repository does not have license file
     licenses.add(Licenses.fromGitHub("gabrielemariotti/changeloglib", Licenses.LICENSE_APACHE_V2));
+
+    licenses.add(Licenses.fromGitHubApacheV2("jakewharton/butterknife"));
     licenses.add(Licenses.fromGitHubApacheV2("hdodenhof/CircleImageView"));
     licenses.add(Licenses.fromGitHubApacheV2("google/dagger"));
-    licenses.add(Licenses.fromGitHubBSD("bumptech/glide", Licenses.FILE_NO_EXTENSION));
     licenses.add(Licenses.fromGitHubApacheV2("evant/gradle-retrolambda"));
     licenses.add(Licenses.fromGitHubApacheV2("google/gson", Licenses.FILE_NO_EXTENSION));
     licenses.add(Licenses.fromGitHubApacheV2("immutables/immutables", Licenses.FILE_NO_EXTENSION));
-    licenses.add(Licenses.fromGitHubMIT("jhy/jsoup", Licenses.FILE_NO_EXTENSION));
     licenses.add(Licenses.fromGitHubApacheV2("square/leakcanary"));
     licenses.add(Licenses.fromGitHubApacheV2("square/okhttp"));
     licenses.add(Licenses.fromGitHubApacheV2("realm/realm-java", Licenses.FILE_NO_EXTENSION));
     licenses.add(Licenses.fromGitHubApacheV2("yqritc/RecyclerView-MultipleViewTypesAdapter",
         Licenses.FILE_NO_EXTENSION));
     licenses.add(Licenses.fromGitHubApacheV2("yshrsmz/LicenseAdapter", Licenses.FILE_NO_EXTENSION));
+    licenses.add(Licenses.fromGitHubApacheV2("jakewharton/threetenabp"));
+    licenses.add(Licenses.fromGitHubApacheV2("jakewharton/timber"));
+    licenses.add(Licenses.fromGitHubApacheV2("grandcentrix/tray"));
+    licenses.add(Licenses.fromGitHubApacheV2("twitter/twitter-text", Licenses.FILE_NO_EXTENSION));
+
+    licenses.add(Licenses.fromGitHubMIT("jhy/jsoup", Licenses.FILE_NO_EXTENSION));
+    licenses.add(Licenses.fromGitHubBSD("bumptech/glide", Licenses.FILE_NO_EXTENSION));
+    licenses.add(Licenses.fromGitHubBSD("facebook/stetho", Licenses.FILE_NO_EXTENSION));
 
     // these 2 licenses have different branch name
     licenses.add(
@@ -47,12 +55,6 @@ public class MainActivity extends AppCompatActivity {
             Licenses.FILE_NO_EXTENSION));
     licenses.add(new GitHubLicenseEntry(Licenses.NAME_APACHE_V2, "ReactiveX/RxJava", "1.x/", null,
         Licenses.FILE_NO_EXTENSION));
-
-    licenses.add(Licenses.fromGitHubBSD("facebook/stetho", Licenses.FILE_NO_EXTENSION));
-    licenses.add(Licenses.fromGitHubApacheV2("jakewharton/threetenabp"));
-    licenses.add(Licenses.fromGitHubApacheV2("jakewharton/timber"));
-    licenses.add(Licenses.fromGitHubApacheV2("grandcentrix/tray"));
-    licenses.add(Licenses.fromGitHubApacheV2("twitter/twitter-text", Licenses.FILE_NO_EXTENSION));
 
     LicenseAdapter adapter = new LicenseAdapter(licenses);
     RecyclerView list = (RecyclerView) findViewById(R.id.list);
